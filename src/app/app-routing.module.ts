7a8484216c3b4e05301c65930e03e137ac0575bf
yj <notebook5308@gmail.com>
2022-11-22 21:12:50 +0800

    week1 end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { BooksPageComponent } from './books-page/books-page.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'books',component:BooksPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
